1d4d12c2707817e1e9e06db165eec72e8a0e5b3f
samuel-ferreira-85 <sam85avila@gmail.com>
2023-12-22 18:25:31 -0300

    Criação do microsserviço de pagamentos

---
+++ pagamentosms/src/test/java/com/samuel/pagamentos/PagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
